085f6895e6e9913cb1701a8c3f00f48e680cf450
Mihul Solanki <ashusolanki1947@gmail.com>
2026-01-08 01:58:17 +0530

    download issue

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './Mihul_Singh_Solanki_Resume.pdf'
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
